6aacbfc2b2fe6f18d7a7b24aafe52e2e8030b9d7
Paul <paullj1@gmail.com>
2016-03-28 14:35:59 -0400

    Adding new operators; dynmically choosing target

--- FaultInjection/FaultInjection.cpp
+++ FaultInjection/FaultInjection.cpp
@@ -21,12 +21,11 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[]) {
 
 	// Declarations
-	int pid = 0;
 	__int64* start_addr;
 	DWORD size_of_ntdsa;
 	DWORD aProcesses[1024], cbNeeded, cProcesses;
+	TCHAR *sProcesses[1024];
 	TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
-	HMODULE hmods[1024];
 	unsigned int i;
 
 	// Get All pids
@@ -35,7 +34,13 @@ int _tmain(int argc, _TCHAR* argv[]) {
 		return -1;
 	}
 
-	// Find pid for lsass.exe
+	// Get screen width
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	int dwidth = csbi.srWindow.Right - csbi.srWindow.Left;
+
+	cout << "Running Processes" << endl;
+	printf("%-6s %-*s\n", "PID", dwidth-7, "Process");
 	cProcesses = cbNeeded / sizeof(DWORD);
 	for (i = 0; i < cProcesses; i++) {
 		if (aProcesses[i] != 0) {
@@ -47,128 +52,151 @@ int _tmain(int argc, _TCHAR* argv[]) {
 					GetModuleBaseName(hProc, hMod, szProcessName, sizeof(szProcessName) / sizeof(TCHAR));
 				}
 
-				if (wstring(szProcessName).find(L"lsass.exe") != string::npos) {
-					pid = aProcesses[i];
-				}
+				_tprintf(TEXT("%6u %-*s\n"), aProcesses[i], dwidth-7, szProcessName);
+				sProcesses[i] = szProcessName;
 				CloseHandle(hProc);
 			}
 		}
 	}
 
-	cout << "lsass pid: " << pid << endl;
+	// Which process?
+	int pid = 0;
+	cout << "Into which process would you like to inject faults? [PID]: ";
+	cin >> pid;
 
-	HANDLE h_lsass = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-	if (!h_lsass) {
-		cout << "Failed to open process (are you root?): " << GetLastError() << endl;
+	HANDLE hTarget = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	if (!hTarget) {
+		cout << "Failed to open process (check your privilege): " << GetLastError() << endl;
 		return -1;
 	}
 
 	// Get Process Image File Name
-	char filename[MAX_PATH];
-	if (GetProcessImageFileName(h_lsass, (LPTSTR)&filename, MAX_PATH) == 0) {
-		cout << "Failed to get image file name: " << GetLastError() << endl;
-		CloseHandle(h_lsass);
+	TCHAR *filename = NULL;
+	for (i = 0; i < sizeof(aProcesses); i++) {
+		if (aProcesses[i] == pid) {
+			filename = sProcesses[i];
+		}
+	}
+
+	if (filename == NULL) {
+		cout << "Failed to locate selected PID's name: " << GetLastError() << endl;
+		CloseHandle(hTarget);
 		return -1;
 	}
 
 	// Enumerate modules within process
-	if (EnumProcessModules(h_lsass, hmods, sizeof(hmods), &cbNeeded)) {
+	HMODULE hmods[1024];
+	cout << "DLLs currently loaded in " << filename << ":" << endl;
+	printf("%-4s %-*s\n", "ID", dwidth-5, "Module Name:");
+	if (EnumProcessModules(hTarget, hmods, sizeof(hmods), &cbNeeded)) {
 		for (i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
 			TCHAR szModName[MAX_PATH];
-			if (GetModuleFileNameEx(h_lsass, hmods[i], szModName, sizeof(szModName) / sizeof(TCHAR))) {
-				if (wstring(szModName).find(L"NTDSA.dll") != string::npos) {
-					_tprintf(TEXT("%s\n"), szModName);
-					MODULEINFO lModInfo = { 0 };
-					if (GetModuleInformation(h_lsass, hmods[i], &lModInfo, sizeof(lModInfo))){
-						cout << "\t Base Addr: " << lModInfo.lpBaseOfDll << endl;
-						cout << "\t Entry Point: " << lModInfo.EntryPoint << endl;
-						cout << "\t Size of image: " << lModInfo.SizeOfImage << endl;
-
-						start_addr = (__int64*)lModInfo.lpBaseOfDll;
-						size_of_ntdsa = lModInfo.SizeOfImage;
-					}
-					else {
-						cout << "Failed to Print enumerated list of modules: " << GetLastError() << endl;
-					}
-				}
+			if (GetModuleFileNameEx(hTarget, hmods[i], szModName, sizeof(szModName) / sizeof(TCHAR))) {
+				_tprintf(TEXT("%4d %-*s\n"), i, dwidth-5, szModName);
 			} else {
 				cout << "Failed to Print enumerated list of modules: " << GetLastError() << endl;
 			}
 		}
-	}
-	else {
+	} else {
 		cout << "Failed to enum the modules: " << GetLastError() << endl;
 	}
 
-	// Ready to continue?
-	string cont = "";
-	cout << "Continue? [Y|n]: ";
-	getline(cin, cont);
-	if (cont.find("n") != string::npos || cont.find("N") != string::npos) {
-		CloseHandle(h_lsass);
-		return 0;
+	// Which Module?
+	int mod_id = 0;
+	cout << "Into which module would you like to inject faults? [ID]: ";
+	cin >> mod_id;
+
+	MODULEINFO lModInfo = { 0 };
+	cout << "Dll Information:" << endl;
+	if (GetModuleInformation(hTarget, hmods[mod_id], &lModInfo, sizeof(lModInfo))){
+		cout << "\t Base Addr: " << lModInfo.lpBaseOfDll << endl;
+		cout << "\t Size of image: " << lModInfo.SizeOfImage << endl << endl;
+
+		start_addr = (__int64*)lModInfo.lpBaseOfDll;
+		size_of_ntdsa = lModInfo.SizeOfImage;
 	}
 
 	byte *buf = (byte *)malloc(size_of_ntdsa);
 	if (!buf) {
 		cout << "Failed to allocate space for memory contents: " << GetLastError() << endl;
-		CloseHandle(h_lsass);
+		CloseHandle(hTarget);
 		return -1;
 	}
 
 	SIZE_T num_bytes_read = 0;
 	int count = 0;
 
-	if (ReadProcessMemory(h_lsass, start_addr, buf, size_of_ntdsa, &num_bytes_read) != 0) {
-		cout << "Read success. Got " << num_bytes_read << " bytes: " << endl;
+	if (ReadProcessMemory(hTarget, start_addr, buf, size_of_ntdsa, &num_bytes_read) != 0) {
+		cout << "Buffered memory contents. Got " << num_bytes_read << " bytes." << endl << endl;
 	} else {
 		int error_code = GetLastError();
 		if (error_code == 299) {
 			cout << "Partial read. Got " << num_bytes_read << " bytes: " << endl;
 		} else  {
 			cout << "Failed to read memory: " << GetLastError() << endl;
-			CloseHandle(h_lsass);
+			CloseHandle(hTarget);
 			free(buf);
 			return -1;
 		}
 	}
 
 	// Write DLL to disk for static ref
+	cout << "Writing static copy of memory contents for analysis to 'C:\\memdump.dll'" << endl;
 	if (num_bytes_read > 0) {
 		FILE *fp;
-		fopen_s(&fp, "C:\\ntdsa_new.dll", "w");
+		fopen_s(&fp, "C:\\memdump.dll", "w");
 		SIZE_T bytes_written = 0;
 		while (bytes_written < num_bytes_read) {
 			bytes_written += fwrite(buf, 1, num_bytes_read, fp);
 		}
 		fclose(fp);
-		cout << "Wrote " << bytes_written << " bytes." << endl;
+		cout << "Wrote " << bytes_written << " bytes." << endl << endl;
+	}
+
+	// Ready to continue?
+	string cont = "";
+	cout << "Ready to begin fault load.  Continue? [Y|n]: ";
+	cin >> cont;
+	if (cont.find("n") != string::npos || cont.find("N") != string::npos) {
+		CloseHandle(hTarget);
+		return 0;
 	}
 
 
+
 	int offset_of_function = 0;
-	if (find_pattern(start_pattern_3, sizeof(start_pattern_3), buf, size_of_ntdsa, 0, &offset_of_function)) {
+
+	// Skip first function
+	find_pattern(start_pattern_6, sizeof(start_pattern_6), buf, size_of_ntdsa, 0, &offset_of_function);
+
+	if (find_pattern(start_pattern_6, sizeof(start_pattern_6), buf, size_of_ntdsa, offset_of_function, &offset_of_function)) {
 		int offset_of_exit = 0;
-		if (find_pattern(end_pattern_3, sizeof(end_pattern_3), buf, size_of_ntdsa, offset_of_function, &offset_of_exit)){
+		if (find_pattern(end_pattern_6, sizeof(end_pattern_6), buf, size_of_ntdsa, offset_of_function, &offset_of_exit)){
+
 			int size_of_function = offset_of_exit - offset_of_function;
 			int call_offset = 0;
-			if (find_pattern(call_addr, sizeof(call_addr), buf, size_of_ntdsa, offset_of_function, &call_offset)) {
-				byte nop_array[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+
+			if (find_pattern(omva_1, sizeof(omva_1), buf, size_of_ntdsa, offset_of_function, &call_offset)) {
+				byte nop_array[] = { 0x90, 0x90, 0x90, 0x90, 0x90 };
 				memcpy(buf + call_offset, nop_array, sizeof(nop_array));
 
 				SIZE_T mem_bytes_written = 0;
-				if (WriteProcessMemory(h_lsass, start_addr, buf, size_of_ntdsa, &mem_bytes_written) != 0)
+				if (WriteProcessMemory(hTarget, start_addr, buf, size_of_ntdsa, &mem_bytes_written) != 0)
 					cout << "Failed to write to memory: " << GetLastError() << endl;
-				else
-					cout << "Wrote " << mem_bytes_written << " bytes... prepare for blue screen." << endl;
+				else {
+					cout << "Successful injection. Prepare for blue screen." << endl;
+					CloseHandle(hTarget);
+					free(buf);
+					return 0;
+				}
 			}
 		}
 	}
 
-	CloseHandle(h_lsass);
+	cout << "Failed to find injection point" << endl;
+	CloseHandle(hTarget);
 	free(buf);
-
-	return 0;
+	return -1;
 }
 
 // Search 'buf' for 'pattern' at 'start'.  If found, sets 'offset', and returns true.
